d9624c56b8c4b4300e8235a15defa2d39e19b02b
Abhimanyu Singh <abhimanyu22nits@gmail.com>
2023-09-09 00:05:29 +0530

    patterns1, C1, optimisation

---
+++ Learn The Basics/Patterns/2.py
@@ -0,0 +1,19 @@
+'''
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+'''
+
+
+
+
+
+
+def printTriangle(self, N):
+        # Code here
+        for i in range(0, N):
+            for j in range(0, i+1):
+                print('*', end = ' ')
+            print()
