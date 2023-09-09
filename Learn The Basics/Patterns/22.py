ef62f21ab40d3abebc85adb26043d47ee2ebab3f
Abhimanyu Singh <abhimanyu22nits@gmail.com>
2023-09-10 00:34:20 +0530

    patters, C11

---
+++ Learn The Basics/Patterns/22.py
@@ -0,0 +1,21 @@
+'''
+
+4 4 4 4 4 4 4
+4 3 3 3 3 3 4
+4 3 2 2 2 3 4
+4 3 2 1 2 3 4
+4 3 2 2 2 3 4
+4 3 3 3 3 3 4
+4 4 4 4 4 4 4
+
+'''
+
+def printTriangle(self, N):
+    for i in range(2*N-1):
+        for j in range(2*N-1):
+            top=i
+            left=j
+            right=(2*N-2)-j
+            down=(2*N-2)-i
+            print(N-min(min(top, down), min(left, right)),"", end="")
+        print()
