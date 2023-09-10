8de6630811d29dd2b3c89b589b180196e1d19d35
Abhimanyu Singh <abhimanyu22nits@gmail.com>
2023-09-10 23:55:48 +0530

    Basic Maths, C3

---
+++ Learn The Basics/Know Basic Maths/Pallindrome.py
@@ -0,0 +1,8 @@
+def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        num = str(x)
+        if(num == num[::-1]):
+            return True
+        else:
+            return False
