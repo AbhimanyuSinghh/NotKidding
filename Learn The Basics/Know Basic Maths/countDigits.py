b31882942502e7b2d81967d2cae7d9a55e80a956
Abhimanyu Singh <abhimanyu22nits@gmail.com>
2023-09-10 23:11:30 +0530

    Basic Maths, C1(str)

--- Learn The Basics/Know Basic Maths/countDigits.py
+++ Learn The Basics/Know Basic Maths/countDigits.py
@@ -1,4 +1,5 @@
 def countDigits(n: int) -> int:
+    '''
     count = 0
     num = n
     while(num!=0):
@@ -6,4 +7,13 @@ def countDigits(n: int) -> int:
         if(p!=0 and n%p==0):
             count+=1
         num//=10
+    return count
+    '''
+    count = 0
+    num = str(n)
+    for i in num:
+        i = int(i)
+        num = int(num)
+        if i!=0 and num%i==0:
+            count+=1
     return count
