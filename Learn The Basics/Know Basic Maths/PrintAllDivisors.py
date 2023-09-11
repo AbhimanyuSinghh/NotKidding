38e8c56a69ff822d19c46f214ab4c6bc467200e2
Abhimanyu Singh <abhimanyu22nits@gmail.com>
2023-09-11 13:29:28 +0530

    Basic Maths, C6, corrected

---
+++ Learn The Basics/Know Basic Maths/PrintAllDivisors.py
@@ -0,0 +1,13 @@
+def sumOfAllDivisors(n: int) -> int:
+    # Write your code here
+    
+    totalSum = 0
+    
+    for i in range(1, n+1):
+        divisorSum = 0
+        for j in range(1, i+1):
+            if(i%j==0):
+                divisorSum += j
+        totalSum += divisorSum
+
+    return totalSum
