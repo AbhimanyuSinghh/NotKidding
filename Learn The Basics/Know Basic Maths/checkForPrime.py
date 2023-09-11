6d6d267844b6f6e4f26fb6b05a2ee44fd2cc234d
Abhimanyu Singh <abhimanyu22nits@gmail.com>
2023-09-11 13:46:53 +0530

    Basic Maths, C7

--- Learn The Basics/Know Basic Maths/checkForPrime.py
+++ Learn The Basics/Know Basic Maths/checkForPrime.py
@@ -1,14 +1,10 @@
+def isPrime(n):
+    for i in range(2, n/2):
+        if n % i == 0:
+            return False
+    return True
 
-def sumOfAllDivisors(n: int) -> int:
-    # Write your code here
-    
-    totalSum = 0
-    
-    for i in range(1, n+1):
-        divisorSum = 0
-        for j in range(1, i+1):
-            if(i%j==0):
-                divisorSum += j
-        totalSum += divisorSum
 
-    return totalSum
+
+
+
